a8815aeba8c2acd2ff8e6c6d72e31204284776dc
Miguel Silva <a74601@alunos.uminho.pt>
2019-02-24 02:38:04 +0000

    Update deployment script

--- publish.sh
+++ publish.sh
@@ -18,13 +18,13 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo -t dream
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
